334e56f29d7c138cb55ed2f5f1825acd6e73753f
Michal Mokran` <michal.mokran@gmail.com>
2020-01-18 11:48:21 +0100

    wss

--- connectfour/public/scripts/mouse_events.js
+++ connectfour/public/scripts/mouse_events.js
@@ -6,7 +6,7 @@ let showPointerInColumn = function(column){
     };
 };
 
-var socket = new WebSocket("ws://connect-four69.herokuapp.com/");
+var socket = new WebSocket("wss://connect-four69.herokuapp.com/");
 
 let my_turn = false;
 let emptyCircle = 'public/images/circle.png';
